ec509fec70209b4713fc2b797c23668405fa043d
artimonist <artimonist@gmail.com>
2025-08-28 02:09:09 +0000

    impl transform command

--- src/commands/transform.rs
+++ src/commands/transform.rs
@@ -1,40 +1,42 @@
 use crate::commands::Execute;
+use disguise::{Mnemonic, Transform};
 
 #[derive(clap::Parser, Debug)]
 pub struct TransformCommand {
-    /// The mnemonic to transform.
+    /// The mnemonic or wif to transform.
     #[clap(value_name = "MNEMONIC|PRIVATE KEY")]
-    pub mnemonic: String,
-
-    #[command(flatten)]
-    pub target: Target,
-
-    /// Option password to decrypt wif.
-    #[clap(hide = true, long = "password")]
-    pub password: Option<String>,
+    pub source: Source,
 }
 
-#[derive(clap::Args, Debug)]
-#[group(required = false, multiple = false)]
-pub struct Target {
-    /// Transform to wallet's private key or encrypted private key
-    #[clap(long, visible_alias = "wif", value_name = "ENCRYPT")]
-    pub wallet: Option<bool>,
+#[derive(Debug, Clone)]
+pub enum Source {
+    Mnemonic(Mnemonic),
+    Wif(String),
+}
 
-    /// Transform to mnemonic
-    #[clap(long)]
-    pub mnemonic: bool,
+impl std::str::FromStr for Source {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.parse::<Mnemonic>() {
+            Ok(x) => Ok(Source::Mnemonic(x)),
+            Err(_) => Ok(Source::Wif(s.to_string())),
+        }
+    }
 }
 
 impl Execute for TransformCommand {
     fn execute(&self) -> anyhow::Result<()> {
-        // let mnemonic = Mnemonic::from_str(&self.mnemonic)?;
-        // let language = match self.language {
-        //     Some(lang) => lang,
-        //     None => select_language(Language::all())?,
-        // };
-        // let mnemonic_transformed = encrypt_mnemonic(&mnemonic, language)?;
-        // println!("Transformed mnemonic: {}", mnemonic_transformed.to_string());
+        match &self.source {
+            Source::Mnemonic(mnemonic) => {
+                let wif = mnemonic.to_string().mnemonic_to_wif()?;
+                println!("{wif}");
+            }
+            Source::Wif(wif) => {
+                let original = wif.mnemonic_from_wif()?;
+                println!("{original}");
+            }
+        }
         Ok(())
     }
 }
